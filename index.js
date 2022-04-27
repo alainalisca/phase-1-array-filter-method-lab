8162bb5a50c5f9c8701cffaa700b0f59cf8b12cc
Alain Alisca <alainalisca@gmail.com>
2022-04-27 06:03:47 -0400

    commit tree 1625ef140bda03aaa2b9f06fa816cac1b0703261

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(origin, found){
+    return origin.filter( 
+        (matchingName) => matchingName.toLowerCase() === found.toLowerCase()
+    )
+}
+function fuzzyMatch(origin, someString){
+    return origin.filter(
+        (matchingName) => matchingName.toLowerCase().indexOf(someString.toLowerCase()) === 0
+    )
+    }
+function matchName(origin, foundName){
+    return origin.filter((filed) => filed.name === foundName)
+}
+
+
